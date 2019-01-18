5424b1a5b3543f448e40b4fbeed94221345bd636
snk <ivan.zakirov@gmail.com>
2019-01-19 00:07:59 +0300

    Создание начального каркаса и проекта для тестов.

---
+++ Code/Start.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NFilesManager.Code
+{
+    class Start
+    {
+        //--Конструкторы/инициализаторы-----------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+
+        //--Свойства------------------------------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+
+        //--События-------------------------------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+
+        //--Методы--------------------------------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором 
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("NFilesManager")]
+[assembly: AssemblyDescription("Network files manager.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("snk (ZIG)")]
+[assembly: AssemblyProduct("NFilesManager.Properties")]
+[assembly: AssemblyCopyright("© Ivan G. Zakirov")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для COM-компонентов.  Если необходим доступ к типу в этой сборке из 
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a2489897-7de4-4862-a513-679ff740f967")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
