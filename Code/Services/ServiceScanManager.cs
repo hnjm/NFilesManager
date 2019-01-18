2f1d7de1c6b94915e49ce85005a0a3203f621ab4
snk <ivan.zakirov@gmail.com>
2019-01-19 00:08:54 +0300

    Игнор лишних файлов.

---
+++ Code/Services/ServiceScanManager.cs
@@ -0,0 +1,55 @@
+﻿using NFilesManager.Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFilesManager.Code.Services
+{
+    /// <summary>
+    /// Менеджер сканирования.
+    /// </summary>
+    public class ServiceScanManager
+    {
+        //--Конструкторы/инициализаторы-----------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+
+        //--Свойства------------------------------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+
+        //--События-------------------------------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// --
+        /// </summary>
+        /// 
+
+        //--Методы--------------------------------------
+        // --
+        //----------------------------------------------
+
+        /// <summary>
+        /// Создать задачу сканирования.
+        /// </summary>
+        /// 
+        public TaskScan CreateTask(string Path) {
+            throw new NotImplementedException();
+        }
+
+    }
+}
